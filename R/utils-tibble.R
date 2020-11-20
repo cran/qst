0f5218f89a93a0fc56bc1f6e83130f59e4ed04ad
Magnus Thor Torfason <m@zulutime.net>
2020-11-20 09:00:02 +0000

    version 0.1.2

---
+++ R/utils-tibble.R
@@ -0,0 +1,10 @@
+#' Load package for awareness of tidy tables.
+#'
+#' See \code{tibble::\link[tibble:tibble]{tibble}} for details.
+#'
+#' @name tibble
+#' @rdname tibble
+#' @keywords internal
+#' @export
+#' @importFrom tibble tibble
+NULL
